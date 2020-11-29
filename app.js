34f4e12548b18a69e9fde3349bdad1c6d9cb8e71
Lukas Kleinert <lklkleinert@gmail.com>
2020-11-29 19:32:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+server.listen(2000);
+console.log("Server started");
+
+const SOCKET_LIST = {};
+
+const io = require('socket.io')(server, {'pingTimeout': 10000, 'pingInterval': 3000});
+io.sockets.on('connection', socket => {
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+    console.log('socket connection: ' + socket.id);
+
+    socket.on('pack_users', () => {
+        pack = [];
+        for (var i in SOCKET_LIST){
+            pack.push({
+                id:i
+            });
+        }
+        socket.emit('user_list', pack);
+    });
+
+    console.log(socket.request.connection.remoteAddress + ' ' + socket.request.connection.remotePort);
+
+    socket.on('disconnect', param => {
+        console.log('socket disconnection: ' + socket.id);
+        delete SOCKET_LIST[socket.id];
+    });
+});
---
+++ client/user_list.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+const buttonfunc = function(){
+    socket.emit('pack_users');
+};
+
+socket.on('user_list', list => {
+    const msgBox = document.getElementById('messages');
+    for (let i = msgBox.childNodes.length-1; i > 0 ; i--){
+        msgBox.removeChild(msgBox.childNodes[i]);
+    }
+    for (const i in list){
+        const msgBox = document.getElementById('messages');
+        const node = document.createElement("li");
+        node.appendChild(document.createTextNode("" + list[i].id));
+        msgBox.appendChild(node);
+    }
+});
