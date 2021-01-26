f8f157d08c37baea800afe0b7b161e56349cf12a
P6y <phyrick@gmx.net>
2021-01-26 19:22:21 +0100

    CLEANUP | Unify style

--- client/navbar.js
+++ client/navbar.js
@@ -2,13 +2,12 @@ const nav = document.getElementById('navbar')
 const navButton = document.getElementById('navbtn')
 const closeButton = document.getElementById('topButton')
 
-navButton.addEventListener('click', () => {
-    // openNav()
-    nav.style.width = '16rem';
-});
-
+navButton.addEventListener('click', () => openNav());
 closeButton.addEventListener('click', () => closeNav());
 
+function openNav() {
+    nav.style.width = '16rem';
+}
 function closeNav() {
     nav.style.width = '0';
 }
