a49a78b31941ef3302d327a3a5c142dd9323bc7f
P6y <phyrick@gmx.net>
2021-01-26 17:30:39 +0100

    TASK | Simplify canvas logic

--- client/canvas.js
+++ client/canvas.js
@@ -3,39 +3,32 @@ const ctx = canvas.getContext('2d');
 
 let last_point = null;
 
+ctx.lineCap = 'round';
+ctx.lineWidth = 5;
+
+canvas.addEventListener('mousedown', ({layerX, layerY}) => {
+    last_point = {x: layerX, y: layerY};
+    draw(layerX, layerY);
+});
+
+canvas.addEventListener('mousemove', ({layerX, layerY}) => {
+    if (last_point != null) {
+        draw(layerX, layerY);
+        last_point = {x: layerX, y: layerY};
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    last_point = null;
+});
+
+canvas.addEventListener('mouseout', () => {
+    last_point = null;
+});
+
 function draw(toX, toY) {
-    ctx.lineCap = 'round';
-    ctx.lineWidth = 5;
     ctx.moveTo(last_point.x, last_point.y);
     ctx.lineTo(toX, toY);
     ctx.stroke();
 }
-
-
-canvas.addEventListener('mousedown', (param) => {
-    last_point = {x:param.layerX, y:param.layerY};
-    draw(param.layerX, param.layerY);
-});
-
-canvas.addEventListener('mousemove', (param) => {
-    if (last_point != null) {
-        draw(param.layerX, param.layerY);
-        last_point.x = param.layerX;
-        last_point.y = param.layerY;
-    }
-});
-
-canvas.addEventListener('mouseup', (param) => {
-    if (last_point != null){
-        draw(param.layerX, param.layerY);
-    }
-    last_point = null;
-});
-
-canvas.addEventListener('mouseout', (param) => {
-    if (last_point != null){
-        draw(param.layerX, param.layerY);
-    }
-    last_point = null;
-});
 
